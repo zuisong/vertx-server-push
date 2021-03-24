00b1e5d10cfceeafe0fdd3b6ea2d2ac6780b476a
chenjian <com.me@foxmail.com>
2021-03-25 00:13:52 +0800

    升级依赖版本

--- src/test/java/test.kt
+++ src/test/java/test.kt
@@ -6,10 +6,10 @@ import java.time.*
 // 定时往集群丢消息 测试消息下发用 配合压力测试脚本
 
 suspend fun main() {
-    val vertx = Vertx.clusteredVertx(VertxOptions()).await()
+  val vertx = Vertx.clusteredVertx(VertxOptions()).await()
 
 
-    vertx.setPeriodic(5_000) {
-        vertx.eventBus().publish("123", JsonObject().put("time", LocalDateTime.now().toString()))
-    }
+  vertx.setPeriodic(5_000) {
+    vertx.eventBus().publish("123", JsonObject().put("time", LocalDateTime.now().toString()))
+  }
 }
