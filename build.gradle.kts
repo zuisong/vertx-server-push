7e911e1b5e19e5925316de40f962345b82bc31e3
zuisong <com.me@foxmail.com>
2022-09-28 17:33:31 +0800

    日志输出json

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,7 @@ dependencies {
   implementation("io.vertx:vertx-stomp")
   implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
   implementation("ch.qos.logback:logback-classic:1.4.1")
-  val logback_json_version = "0.1.5"
-  implementation("ch.qos.logback.contrib:logback-jackson:$logback_json_version")
-  implementation("ch.qos.logback.contrib:logback-json-classic:$logback_json_version")
+  implementation("net.logstash.logback:logstash-logback-encoder:7.2")
 
 
 }
