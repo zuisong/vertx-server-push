f8de126f20ad37da007f9e5d8a39973ebb6b32e2
zuisong <com.me@foxmail.com>
2022-10-15 12:15:23 +0800

    upgrade to jdk17

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 dependencies {
   implementation(platform("io.vertx:vertx-dependencies:4.3.4"))
   implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:1.7.20"))
-  implementation(enforcedPlatform("com.fasterxml.jackson:jackson-bom:2.13.3"))
+  implementation(enforcedPlatform("com.fasterxml.jackson:jackson-bom:2.13.4"))
 
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("com.fasterxml.jackson.core:jackson-databind")
@@ -25,16 +25,17 @@ dependencies {
   implementation("io.vertx:vertx-lang-kotlin-coroutines")
   implementation("io.vertx:vertx-stomp")
   implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
-  implementation("ch.qos.logback:logback-classic:1.4.3")
+  implementation("ch.qos.logback:logback-classic:1.4.4")
   implementation("net.logstash.logback:logstash-logback-encoder:7.2")
 }
 
 group = "cn.mmooo"
 version = "1.0.0"
 description = "vertx-server-push"
-java.sourceCompatibility = JavaVersion.VERSION_17
 
 java {
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
   withSourcesJar()
 }
 
@@ -52,5 +53,6 @@ application {
 
 tasks.withType<KotlinCompile> {
   kotlinOptions.useK2 = true
+  kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
 }
 
--- src/main/java/cn/mmooo/Main.kt
+++ src/main/java/cn/mmooo/Main.kt
@@ -2,14 +2,19 @@ package cn.mmooo
 
 import cn.mmooo.verticle.*
 import io.vertx.core.*
+import mu.*
 
+private val logger = KotlinLogging.logger { }
 
 fun main() {
   val vertx = Vertx.vertx()
+  println("hello world")
   vertx.deployVerticle(
     { StompBridgeVerticle() },
     DeploymentOptions().apply {
       instances = 8
     })
+
+  logger.info { "aaaa" }
 
 }
